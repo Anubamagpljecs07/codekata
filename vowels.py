89b9e7b23f4fe743cf249aeea522e18cc32777a7
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-01-07 15:31:19 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,8 @@
+c=input()
+if c.isalpha():
+	if c=="a" or c=="e" or c=="i" or c=="o" or c=="u" or c=="A" or c=="E" or c=="I" or c=="U":
+		print("Vowel")
+	else:
+		print("Consonant")
+else:
+	print("invalid")
