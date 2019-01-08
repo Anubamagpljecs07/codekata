b087e9a0ead483ccf6197c197bb9494395fdcf93
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-01-08 18:47:59 +0530

    Update Nmultiples.py

--- Nmultiples.py
+++ Nmultiples.py
@@ -1,8 +1,13 @@
 n=int(input())
+k=0
 c=0
 for j in range(n,10000000):
         if j%n==0:
+            k=k+1
             c+=1
-            print(j,end=" ")
-            if c==5:
+            if c==1:
+                print(j,end="")
+            else:
+                print("",j,end="")
+            if k==5:
                 break
