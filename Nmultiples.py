c01a3a2928d051432043292ed2e6adf7766d66b7
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-01-08 18:51:30 +0530

    Update Nmultiples.py

--- Nmultiples.py
+++ Nmultiples.py
@@ -1,13 +1,16 @@
 n=int(input())
 k=0
 c=0
-for j in range(n,10000000):
-        if j%n==0:
-            k=k+1
-            c+=1
-            if c==1:
-                print(j,end="")
-            else:
-                print("",j,end="")
-            if k==5:
-                break
+if n==0:
+        print(0,0,0,0,0)
+else:
+        for j in range(n,10000000):
+                if j%n==0:
+                    k=k+1
+                    c+=1
+                    if c==1:
+                        print(j,end="")
+                    else:
+                        print("",j,end="")
+                    if k==5:
+                        break
