cc5392947956412614a98185745fa2044d099b8c
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-01-08 18:18:48 +0530

    Update Neven.py

--- Neven.py
+++ Neven.py
@@ -1,5 +1,10 @@
 n,m=(map(int,input().split()))
+c=0
 for i in range(n+1,m):
     if i%2==0:
-        print(i,end=" ")
+        c+=1
+        if c==1:
+            print(i,end="")
+        else:
+            print("",i,end="")
         
