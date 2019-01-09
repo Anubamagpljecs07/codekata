d2937a2e122313f2fea31837052e6a120d95225f
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-01-09 10:36:28 +0000

    Create Fib.py

---
+++ Fib.py
@@ -0,0 +1,46 @@
+a,b,c,d=0,1,0,0
+f=[]
+n=int(input())
+if n!=1:
+	while(c<n):
+		d=a+b
+		a,b=b,da,b,c,d=0,1,0,0
+f=[]
+n=int(input())
+if n!=1:
+	while(c<n):
+		d=a+b
+		a,b=b,d
+		c+=1
+		f.append(a)
+	for i in range(0,len(f)):
+		if i==0:
+			print(f[i],end="")
+		else:
+			print("",f[i],end="")
+else:
+	print(1)
+		c+=1
+		f.append(a)
+	for i in range(0,len(f)):
+		if i==0:
+			print(f[i],end="")
+		else:
+			print("",f[i],end="")a,b,c,d=0,1,0,0
+f=[]
+n=int(input())
+if n!=1:
+	while(c<n):
+		d=a+b
+		a,b=b,d
+		c+=1
+		f.append(a)
+	for i in range(0,len(f)):
+		if i==0:
+			print(f[i],end="")
+		else:
+			print("",f[i],end="")
+else:
+	print(1)
+else:
+	print(1)
