7cf1e298265e24b97513decfb62157ea18215a88
Anubamagpljecs07 <46440793+Anubamagpljecs07@users.noreply.github.com>
2019-01-22 00:48:06 +0530

    Update and rename Oddpositions.py to Oddnums.py

--- Oddnums.py
+++ Oddnums.py
@@ -1,15 +1,15 @@
 n=input()
 def isodd(x):
-    if x%2==1:
+    if int(x)%2==1:
         a="O"
     else:
         a="E"
     return a
 c=0
-for i in range(0,len(n)):
+for i in n:
     if isodd(i)=="O":
         c+=1
         if c==1:
-            print(n[i],end="")
+            print(i,end="")
         else:
-            print("",n[i],end="")
+            print("",i,end="")
